5396dea4085feb78046ca05e7f0237862bd3c259
Melanie Grosso <grosso.melanie@gmail.com>
2018-04-30 15:02:27 +0200

    Mise en place de l'environnement. Vérification avec Hello World.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Vérification de la mise en place de l'environnement
+    cout<< "Hello World !" <<endl;
+    cout<< " " <<endl;
+    
+    return 0;
+    
+}
