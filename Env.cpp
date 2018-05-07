e85062124d0d32d7e260fb805a798ced4a8cdfb9
gsell <gselllouise@yahoo.fr>
2018-05-07 16:21:16 +0200

    Un petit début bien sympa

--- Env.cpp
+++ Env.cpp
@@ -3,6 +3,8 @@
 // =============================================================================
 #include "Env.h"
 #include "Case.h"
+#include <iostream>
+using namespace std;
 
 // =============================================================================
 //                              Constructors
@@ -15,7 +17,6 @@ Env::Env(){
   int height_=32;
   int temps_simul=5000;
   Case ** grille = new Case * [height_]; // [n] réserve n élément du type qui précède
-  int i;
   for(int i=0;i<height_;++i){
     grille[i] = new Case [width_];
   }
@@ -26,5 +27,31 @@ Env::Env(){
 // =============================================================================
 
 // =============================================================================
-//                         Public function memebers
+//                         Public function members
 // =============================================================================
+void Env::etat(){
+  int s=0;
+  int l=0;
+  for(int i=0;i<height_;++i){
+    for(int j=0;j<width_;++j){
+      if (grille[i,j]->p_bact()->type()=="S"){
+        s+=1;
+      }
+      else if (grille[i,j]->p_bact()->type()=="L"){
+        l+=1;
+      }
+    }
+  }
+  if(s==0){
+    if (l==0){
+      cout << "Extinction" << endl;
+    }
+    else {
+      cout << "Exclusion" << endl;
+    }
+  }
+  else {
+    cout << "Cohabitation" << endl;
+  }  
+}
+
