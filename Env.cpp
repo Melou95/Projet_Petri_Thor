bb59d52e8896ef614757909efa6334d07d5736ee
gsell <gselllouise@yahoo.fr>
2018-05-07 18:06:33 +0200

    Cool

--- Env.cpp
+++ Env.cpp
@@ -9,16 +9,10 @@ using namespace std;
 // =============================================================================
 //                              Constructors
 // =============================================================================
-Env::Env(){
-  int T_=1;
-  float D_=0.1;
-  int Ainit_=25;
-  int width_=32;
-  int height_=32;
-  int temps_simul=5000;
-  Case ** grille = new Case * [height_]; // [n] réserve n élément du type qui précède
+Env::Env() : T_(1), D_(0.1), Ainit_(25), width_(32), height_(32), temps_simul_(5000){
+  Case ** grille_ = new Case * [height_]; // [n] réserve n élément du type qui précède
   for(int i=0;i<height_;++i){
-    grille[i] = new Case [width_];
+    grille_[i] = new Case [width_];
   }
   
 }
@@ -28,10 +22,49 @@ Env::Env(){
 Env::~Env(){
   for(int i=0;i<height_;++i){
     for(int j=0;j<width_;++j){
-      delete grille[i,j];
+      delete grille_[i,j];
     }
   }
 }
+// ===========================================================================
+//                                Getters
+// ===========================================================================
+Case ** Env::grille(){
+  return grille_;
+}
+
+int Env::T(){
+  return T_;
+}
+  
+float Env::D(){
+  return D_;
+}
+
+int Env::Ainit(){
+  return Ainit_;
+}
+
+
+int Env::width(){
+  return width_;
+}
+
+
+int Env::height(){
+  return height_;
+}
+
+
+int Env::temps_simul(){
+  return temps_simul_;
+}
+// ===========================================================================
+//                                Setters
+// ===========================================================================
+  
+  
+  
 // =============================================================================
 //                         Public function members
 // =============================================================================
@@ -40,10 +73,10 @@ void Env::etat(){
   int l=0;
   for(int i=0;i<height_;++i){
     for(int j=0;j<width_;++j){
-      if (grille[i,j]->p_bact()->type()=='S'){
+      if (grille_[i,j]->p_bact()->type()=='S'){
         s+=1;
       }
-      else if (grille[i,j]->p_bact()->type()=='L'){
+      else if (grille_[i,j]->p_bact()->type()=='L'){
         l+=1;
       }
     }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include "Bacterie.h"
+#include "Case.h"
+#include "Env.h"
+#include "L.h"
+#include "S.h"
 using namespace std;
 
 int main(){
     //Vérification de la mise en place de l'environnement
     cout<< "Hello World !\0" <<endl;
     cout<< " " <<endl;
+    
+    Env monde;
+    cout << monde.T() << endl;
+    cout << monde.D() << endl;
     
     return 0;
     
