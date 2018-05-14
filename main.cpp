c5573862378e18eb659cbbc611745c43bc0564b2
gsell <gselllouise@yahoo.fr>
2018-05-14 15:46:09 +0200

    Ajout de tests dans main

--- main.cpp
+++ main.cpp
@@ -11,23 +11,18 @@ int main(){
     cout<< "Hello World !\0" <<endl;
     cout<< " " <<endl;
     
-<<<<<<< HEAD
     S bact1;
     cout << bact1.phenotype()[0] << endl;
     bact1.division();
     cout << bact1.phenotype()[0] << endl;    
-    /*Env monde;
-    cout << monde.T() << endl;
-    cout << monde.D() << endl;*/
     
-=======
     //Pour tester le constructeur de Env
+    cout << "Pour tester le constructeur de Env" << endl; 
     Env monde;
     cout << monde.T() << endl;
     cout << monde.D() << endl;
     monde.initialise();
     monde.etat();
->>>>>>> 98ab3784acdd0d1926b7e8efa6353adc7a9591c6
     
     return 0;
     
