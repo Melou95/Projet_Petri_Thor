49bd3ddc8ac19083c1de73e1ba2d72fa57d186d2
Melanie Grosso <grosso.melanie@gmail.com>
2018-05-14 15:41:29 +0200

    On est bon là

--- main.cpp
+++ main.cpp
@@ -10,24 +10,22 @@ int main(){
     //Vérification de la mise en place de l'environnement
     cout<< "Hello World !\0" <<endl;
     cout<< " " <<endl;
-    
-<<<<<<< HEAD
+
+    // Pour tester le contructeur de S
     S bact1;
     cout << bact1.phenotype()[0] << endl;
+    
+    //Pour tester la méthode division
     bact1.division();
     cout << bact1.phenotype()[0] << endl;    
-    /*Env monde;
-    cout << monde.T() << endl;
-    cout << monde.D() << endl;*/
     
-=======
     //Pour tester le constructeur de Env
     Env monde;
     cout << monde.T() << endl;
     cout << monde.D() << endl;
     monde.initialise();
     monde.etat();
->>>>>>> 98ab3784acdd0d1926b7e8efa6353adc7a9591c6
+
     
     return 0;
     
